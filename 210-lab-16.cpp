7e5cd2f5359c8f7a829f47f9e5f43f02be6e833d
Noel Mier-Luna <noel.lmier@gmail.com>
2026-03-04 20:48:59 -0800

    Initial Commit using Lab 14 Code

---
+++ 210-lab-16.cpp
@@ -0,0 +1,67 @@
+// COMSC-210 | Lab 16 | Noel Mier-Luna
+
+
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int TABLE_WIDTH = 5; // Constant for table width, used in print() function.
+
+class Color {
+    int red;
+    int green; // Private member variables to store color components.
+    int blue;
+
+    public:
+        int getRed() const {
+            return red;
+        }
+    void setRed(int r) {
+        red = r;
+    }
+    int getGreen() const {
+        return green;
+    } // Getter functions const to avoid modifying objects. Used by print() to access private member variables.
+    void setGreen(int g) {
+        green = g;
+    } // Setter functions to modify private member variables.
+    int getBlue() const {
+        return blue;
+    }
+    void setBlue(int b) {
+        blue = b;
+    }
+
+    void print() const {
+        cout << setw(TABLE_WIDTH) << "R: " << getRed() << " " <<
+            setw(TABLE_WIDTH) << "G: " << getGreen() << " " <<
+            setw(TABLE_WIDTH) << "B: " << getBlue() << endl; // Prints color's RGB values.
+    }
+};
+
+int main() {
+    Color Yellow; // RGB values are 255, 224, 32.
+    Yellow.setRed(255);
+    Yellow.setGreen(224); // Setting RGB values for Yellow color using setter functions.
+    Yellow.setBlue(32); // Useful for controlled access to private member variables.
+    cout << "Yellow's RGB values are: " << endl;
+    Yellow.print();
+
+    Color Purple; // RGB values are 160, 32, 255.
+    Purple.setRed(160);
+    Purple.setGreen(32);
+    Purple.setBlue(255);
+    cout << "Purple's RGB values are: " << endl;
+    Purple.print();
+
+    Color MintTeal; // RGB values are 102, 205, 170.
+    MintTeal.setRed(102);
+    MintTeal.setGreen(205);
+    MintTeal.setBlue(170);
+    cout << "Mint-teal's RGB values are: " << endl;
+    MintTeal.print();
+
+    return 0;
+}
